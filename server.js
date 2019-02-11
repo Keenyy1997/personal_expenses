8c64e73c1e1ee490b3f518c82a9a6864c51ae892
Kenny Alberto Vallejo Cobis <Keenyy1997@gmail.com>
2019-02-11 00:13:52 -0500

    agregando los require al server.js

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 
 const app = express()
+const bodyParser = require('body-parser')
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/api", require(__dirname + "/src/server"))
 
 app.use(express.static('dist'))
 
